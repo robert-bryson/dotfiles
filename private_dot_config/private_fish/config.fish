7f2d4285334b7d38c226eb1750e551b80ee07a33
robert-bryson <robert.bryson@gsa.gov>
2021-12-15 15:31:01 -0800

    vimrc: tweaks, syntax enable; config.fish: autoload rbenv

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,3 +1,8 @@
+# Load rbenv automatically
+# https://github.com/rbenv/rbenv#using-package-managers
+status --is-interactive; and rbenv init - fish | source
+
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
