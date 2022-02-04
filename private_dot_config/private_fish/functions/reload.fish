01eed9077e22017fef307045c7bae5228466529b
robert-bryson <robert.bryson@gsa.gov>
2022-02-04 12:33:28 -0800

    fish: added missing functions

---
+++ private_dot_config/private_fish/functions/reload.fish
@@ -0,0 +1,5 @@
+function reload --description 'reloads and sources'
+    # Causes fish to re-read its initialization file.
+    echo 'Sourcing config.fish..'
+    source ~/.config/fish/config.fish
+end
