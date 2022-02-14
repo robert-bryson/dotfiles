0ce276a88b5931c20ea96cc24da4896db7739d49
robert-bryson <robert.bryson@gsa.gov>
2022-02-14 10:21:26 -0800

    small tweaks

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,3 +1,10 @@
+### ENV ###
+
+# might need to change for macos? works on wsl
+# set -x Z_SCRIPT_PATH /home/linuxbrew/.linuxbrew/etc/profile.d/z.sh
+# use fish z plugin: https://github.com/jethrokuan/z
+
+### INTERACTIVE ###
 if status is-interactive
     # Commands to run in interactive sessions
     rbenv init - fish | source
